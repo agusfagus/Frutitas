87f6d6ca96f2e85d0490801293c5cbc5effb0496
Federico Ramundo <federico.ramundo@wolox.com.ar>
2016-12-19 17:21:54 -0300

    menu

---
+++ Assets/Balloon.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Balloon : MonoBehaviour {
+
+	public float force = 100f;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Rigidbody> ().AddForce (force * Vector3.up);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+//		Cardboard.SDK.VRModeEnabled = false;
+		// Disable VR
+	}
+	
+	// Update is called once per frame
+	void Update () {
+//		GameObject particle = new GameObject();
+//			foreach (Touch touch in Input.touches) {
+//				if (touch.phase == TouchPhase.Began) {
+//					// Construct a ray from the current touch coordinates
+//					Ray ray = Camera.main.ScreenPointToRay (touch.position);
+//					if (Physics.Raycast (ray)) {
+//						// Create a particle if hit
+//						Instantiate (particle, transform.position, transform.rotation);
+//					}
+//				}
+//			}
+		}
+
+	public void PlayButton(){
+		Application.LoadLevel ("Main");
+ 		// Disable VR
+	}
+
+	public void  CardboardButton(){
+		Application.LoadLevel ("Main");
+ 		// Enable VR
+	}
+}
