b9aa3f2b7c427011bf172039c7a58fed50ad6189
Federico Ramundo <federico.ramundo@wolox.com.ar>
2016-12-19 16:58:59 -0300

    kickoff

---
+++ Assets/BalloonSpawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalloonSpawner : MonoBehaviour {
+
+	public Transform originObject;
+	public float tickTime = 5.0f;
+
+	private float nextActionTime = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time > nextActionTime ) {
+			nextActionTime += tickTime;
+			Debug.Log ("Repeat");
+		}
+	}
+}
